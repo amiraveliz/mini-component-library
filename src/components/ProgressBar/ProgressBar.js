de66209ea20c89bf2f282d083d80270349957753
Amira Veliz <amira.veliz@gmail.com>
2025-03-07 23:15:34 -0300

    set up styles for progress and select components

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,78 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+//value 0 to 100
+//size small medium large
+const ProgressBar = ({ value = 50, size = "medium" }) => {
+  const containerStyles = ContainerStyles[size];
+  const progressStyles = ProgressStyles[size];
+  return (
+    <>
+      <ProgressWrapper
+        max="100"
+        value={value}
+        size={size}
+        containerStyles={containerStyles}
+        progressStyles={progressStyles}
+      />
+      <VisuallyHidden>Progressbar {value} from 100</VisuallyHidden>
+    </>
+  );
 };
+
+const ContainerStyles = {
+  large: {
+    padding: "4px",
+    height: "24px",
+  },
+  medium: {
+    padding: "0px",
+    height: "12px",
+  },
+  small: {
+    padding: "0px",
+    height: "8px",
+  },
+};
+
+const ProgressStyles = {
+  large: {
+    height: "16px",
+  },
+  medium: {
+    height: "12px",
+  },
+  small: {
+    height: "8px",
+  },
+};
+
+const ProgressWrapper = styled.progress`
+  ${(props) => props.containerStyles}
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+  &[value] {
+    width: ${(props) => props.width};
+    appearance: none;
+    ${(props) => props.progressStyles}
+    ::-webkit-progress-bar {
+      border-radius: 4px;
+      background-color: transparent;
+    }
+    ::-webkit-progress-value {
+      border-radius: 4px;
+      border-top-right-radius: ${(props) => (props.value < 100 ? "0" : "4px")};
+      border-bottom-right-radius: ${(props) =>
+        props.value < 100 ? "0" : "4px"};
+      background-color: ${COLORS.primary};
+    }
+  }
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Container>
+      <SelectWrapper
+        textLength={displayedValue.length + 7}
+        value={value}
+        onChange={onChange}
+      >
+        {children}
+      </SelectWrapper>
+      <IconWrapper id="chevron-down" size="16" />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const IconWrapper = styled(Icon)`
+  position: absolute;
+  top: 50%;
+  right: 16px;
+  transform: translateY(-50%);
+  pointer-events: none; /* Prevent interaction */
+  color: ${COLORS.gray700};
+`;
+
+const SelectWrapper = styled.select`
+  width: ${(props) => props.textLength}ch; /* Dynamically adjust width */
+  min-width: 100px; /* Set a minimum width */
+  max-width: 250px; /* Prevent it from growing too much */
+  padding: 12px 16px;
+  padding-right: 52px; /* Ensure space for the icon */
+  font-size: 16px;
+  border-radius: 8px;
+  border-color: transparent;
+  appearance: none; /* Remove default styles */
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
